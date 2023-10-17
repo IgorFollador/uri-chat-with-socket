e49760b0732757c3d82a343a9b3dfe77e4c7182e
Igor Follador <igorledf@hotmail.com>
2023-10-16 22:32:38 -0300

    Refatoring client tcp using C language

--- client.c
+++ client.c
@@ -1,47 +1,71 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
-#define PORT 8000
+#define SERVER_PORT 8000
+#define SERVER_ADDRESS "localhost"
+#define BUFFER_SIZE 1024
+
+int keepRunning = 1;
+int clientSocket;
+char readBuffer[BUFFER_SIZE];
+
+void *readThreadFunction(void *arg) {
+    while (keepRunning) {
+        int bytesRead = recv(clientSocket, readBuffer, sizeof(readBuffer) - 1, 0);
+        if (bytesRead <= 0) {
+            keepRunning = 0;
+            break;
+        }
+        readBuffer[bytesRead] = '\0';
+        printf("%s\n", readBuffer);
+    }
+    return NULL;
+}
 
 int main() {
-    int clientSocket;
     struct sockaddr_in serverAddr;
-
-    // Cria socket
-    if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket");
-        exit(1);
+    
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0) {
+        perror("Erro ao criar socket");
+        exit(EXIT_FAILURE);
     }
 
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(PORT);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    bzero(&(serverAddr.sin_zero), 8);
+    serverAddr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, SERVER_ADDRESS, &serverAddr.sin_addr);
 
-    // Conecta ao servidor
-    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr)) == -1) {
-        perror("Connect");
-        exit(1);
+    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Erro ao conectar ao servidor");
+        close(clientSocket);
+        exit(EXIT_FAILURE);
     }
 
-    printf("Conectado ao servidor de chat.\n");
-    printf("Digite 'q' e pressione Enter para sair.\n");
+    pthread_t readThread;
+    pthread_create(&readThread, NULL, readThreadFunction, NULL);
 
-    char message[1024];
+    char inputBuffer[BUFFER_SIZE];
 
+    printf("Digite uma mensagem (ou 'sair' para encerrar):\n");
     while (1) {
-        fgets(message, sizeof(message), stdin);
-        if (message[0] == 'q' && message[1] == '\n') {
+        fgets(inputBuffer, sizeof(inputBuffer), stdin);
+        size_t len = strlen(inputBuffer);
+        if (len > 0 && inputBuffer[len - 1] == '\n') {
+            inputBuffer[len - 1] = '\0';
+        }
+
+        if (strcmp(inputBuffer, "sair") == 0) {
+            keepRunning = 0;
             break;
         }
-        send(clientSocket, message, strlen(message), 0);
+        send(clientSocket, inputBuffer, strlen(inputBuffer), 0);
     }
 
+    pthread_join(readThread, NULL);
     close(clientSocket);
 
     return 0;
